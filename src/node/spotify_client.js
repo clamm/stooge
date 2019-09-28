027b4baab51eab634c014985b7d5da12d946769d
Carsten Lamm <CarstenLamm@googlemail.com>
2019-09-28 17:25:30 +0200

    Fix async issue by using Promises and async/await
    We broke the test intentionally, because we didn't run them. :P
    Don't do this!!!
    
    But it works. :D

--- src/node/spotify_client.js
+++ src/node/spotify_client.js
@@ -4,65 +4,69 @@ const request = require("request");
 const querystring = require("querystring");
 const fs = require("fs");
 
-const client_id = "client_id";
-const client_secret = "client_secret";
+const client_id = "";
+const client_secret = "";
 
-// request authorization
-const authOptions = {
-  url: "https://accounts.spotify.com/api/token",
-  headers: {
-    "Authorization": "Basic " + (new Buffer(client_id + ":" + client_secret).toString("base64"))
-  },
-  form: {
-    grant_type: "client_credentials"
-  },
-  json: true
+
+const authed_request = ( { url } ) => {
+  console.log("start authed_request");
+
+  const authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "Authorization": "Basic " + (new Buffer(client_id + ":" + client_secret).toString("base64"))
+    },
+    form: {
+      grant_type: "client_credentials"
+    },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+
+        // use the access token to access the Spotify Web API
+        const token = body.access_token;
+        console.log("get auth token");
+        // console.log(token)
+
+        const options = {
+          url,
+          headers: {
+            "Authorization": "Bearer " + token
+          },
+          json: true
+        };
+        resolve({ options });
+      } else {
+        reject(error);
+      }
+    });
+  });
 };
 
-const error_callback = function(error, response) {
-  console.log("auth request didn't go well: response code " + response.statusCode);
-  throw (error);
-}
 
-// possibly add 2nd callback for error
-const authed_request = function(url, callback) {
-  console.log("start authed_request");
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-
-      // use the access token to access the Spotify Web API
-      var token = body.access_token;
-      console.log("get auth token");
-      // console.log(token)
-
-      var options = {
-        url: url,
-        headers: {
-          "Authorization": "Bearer " + token
-        },
-        json: true
-      };
-      request.get(options, function(error, response, body) {
-        console.log("run authorized get request against API");
-        if (!error && response.statusCode === 200) {
-          callback(body);
-        } else {
-          error_callback(error, response);
-        }
-      });
-    } else {
-      error_callback(error, response);
-    }
+const makeGetRequest = ({ options }) => {
+  return new Promise((resolve, reject) => {
+    request.get(options, (error, response, body) => {
+      console.log("run authorized get request against API");
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
   });
-}
+};
 
 
-const url_builder = function(song_artist) {
+const url_builder = (song_artist) => {
   if (("track" in song_artist) == false || ("artist" in song_artist) == false) {
     throw new Error("no artist or track information: " + song_artist);
   }
-  var base_url = "https://api.spotify.com/v1/search?query=";
-  var options = {
+  const base_url = "https://api.spotify.com/v1/search?query=";
+  const options = {
     "type": "track",
     "offset": 0,
     "limit": 5
@@ -73,22 +77,22 @@ const url_builder = function(song_artist) {
     ...options
   };
 
-  var query_str = querystring.stringify(query);
-  var url = base_url + query_str;
-  return url;
+  const query_str = querystring.stringify(query);
+  return base_url + query_str;
 };
 
 
-const get_top_x = function(body, callback, top_x = 3) {
-  if (typeof body !== "undefined" && body) {
+const get_top_x = (body) => {
+  const top_x = 3;
+  if (body) {
     if ("tracks" in body && "items" in body.tracks) {
       console.log("running get_top_x");
-      var limited = body.tracks.items.slice(0, top_x);
+      const limited = body.tracks.items.slice(0, top_x);
 
-      var local_results = [];
-      limited.forEach(function(item) {
-        var fmt = {
-          "artists": item.artists.forEach(function(a) {
+      let local_results = [];
+      limited.forEach((item) => {
+        const fmt = {
+          "artists": item.artists.forEach((a) => {
             return {
               "name": a.name,
               "type": a.type,
@@ -97,26 +101,25 @@ const get_top_x = function(body, callback, top_x = 3) {
           "name": item.name,
           "uri": item.uri
         }
-        local_results.push(fmt)
+        local_results.push(fmt);
       });
 
-      var top_x_tracks = {
+      const top_x_tracks = {
         "top_3": local_results
       };
-      callback(top_x_tracks);
+      return Promise.resolve(top_x_tracks);
 
     } else {
-      throw new Error("no tracks or items found in body");
+      return Promise.resolve({ "meta": "no tracks or items found in body" });
     }
   } else {
-    throw new Error("search response is undefined");
+    return Promise.resolve({ "meta": "search response is undefined"});
   }
-
 };
 
 
-const save_search_results = function(file_name, top_3_songs) {
-  fs.writeFile(file_name, top_3_songs, function(err) {
+const save_search_results = (file_name, top_3_songs) => {
+  fs.writeFile(file_name, JSON.stringify(top_3_songs), (err) => {
     if (err) {
       throw (err);
     }
@@ -125,8 +128,8 @@ const save_search_results = function(file_name, top_3_songs) {
 };
 
 
-const appender = function(global_results, top_x) {
-  console.log("running appender with global_results:");
+const append_to_global_results = (global_results, top_x) => {
+  console.log("running append_to_global_results with global_results:");
   console.log(global_results);
   console.log("and top_3:");
   console.log(top_x);
@@ -134,33 +137,36 @@ const appender = function(global_results, top_x) {
 };
 
 
+const getEntriesPerSong = (url) => {
+  return authed_request({ url }).then(makeGetRequest).then(get_top_x);
+}
+
+
 // writer is an input because that'll be easier to test
-const do_search_requests = function(input_file, writer) {
+const do_search_requests = async (input_file, writer) => {
   console.log("read input file");
-  var contents = fs.readFileSync(input_file);
-  var jsonContent = JSON.parse(contents);
-  var global_results = [];
-  jsonContent.forEach(function(line) {
+  const contents = fs.readFileSync(input_file);
+  const jsonContent = JSON.parse(contents);
+  let global_results = [];
+
+  for (let i = 0; i < jsonContent.length; i++) {
+    const line = jsonContent[i]
     console.log(line);
-    var url = url_builder(line);
+    const url = url_builder(line);
     console.log(url);
-    // TODO this should happen after internal callbacks returned
-    authed_request(url, function(body) {
-      get_top_x(body, function(top_x) {
-        appender(global_results, top_x);
-      })
-    });
-  });
+    const top_x = await getEntriesPerSong(url);
+    console.log("here:", top_x);
+    append_to_global_results(global_results, top_x);
+  };
   writer(global_results);
 }
 
 
-
 module.exports = {
-  url_builder: url_builder,
-  get_top_x: get_top_x,
-  save_search_results: save_search_results,
-  do_search_requests: do_search_requests
+  do_search_requests,
+  get_top_x,
+  save_search_results,
+  url_builder,
 };
 
 // create playlist related functions
